a7543e56e96ca0df16db54dd5d5296f03e0892ba
faresmnasri <31959540+faresmnasri@users.noreply.github.com>
2018-10-30 18:18:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "2";
+const prefix = "/";
 /////////////////////////
 ////////////////////////
 
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : FALLEGA TEAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`.Help | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`.Help | FALLEGA MUISIC `,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
